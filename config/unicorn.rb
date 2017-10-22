f0fb9dc415844e3a7f2029abcdd48f46f6cc21ec
takeuchi <takeuchi.hrt@gmail.com>
2017-10-22 01:34:59 +0000

    画像表示

---
+++ config/unicorn.rb
@@ -0,0 +1,23 @@
+# loading booster
+preload_app true
+# before starting processes
+before_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if old_pid != server.pid
+    begin
+      Process.kill "QUIT", File.read(old_pid).to_i
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
+end
+# after finishing processes
+after_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  end
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
