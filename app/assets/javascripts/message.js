5c0440e6a6a70a6222fc23290cfabfa0c0535f76
care0717 <dev.care0717@gmail.com>
2017-10-22 13:53:56 +0900

    update: realtime

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,31 @@
+$(function(){
+  function buildpost(post) {
+    var posts = $('tbody').append(post);
+    //'tbody'に'tr'以下のhtml全てをappendする
+  }
+
+  $(function(){
+    setInterval(update, 10000);
+    //10000ミリ秒ごとにupdateという関数を実行する
+  });
+  function update(){ //この関数では以下のことを行う
+    if($('.posts')[0]){ //もし'posts'というクラスがあったら
+      var message_id = $('.posts:last').data('id'); //一番最後にある'posts'というクラスの'id'というデータ属性を取得し、'message_id'という変数に代入
+    } else { //ない場合は
+      var message_id = 0 //0を代入
+    }
+    $.ajax({ //ajax通信で以下のことを行う
+      url: location.href, //urlは現在のページを指定
+      type: 'GET', //メソッドを指定
+      data: { //railsに引き渡すデータは
+        post: { id: post_id } //このような形(paramsの形をしています)で、'id'には'post_id'を入れる
+      },
+      dataType: 'json' //データはjson形式
+    })
+    .always(function(data){ //通信したら、成功しようがしまいが受け取ったデータ（@new_post)を引数にとって以下のことを行う
+      $.each(data, function(i, data){ //'data'を'data'に代入してeachで回す
+        buildpost(data); //buildpostを呼び出す
+      });
+    });
+  }
+});
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
   def index
     @posts = Post.all
+    respond_to do |format|
+      format.html # html形式でアクセスがあった場合は特に何もなし(@messages = Message.allして終わり）
+      format.json { @new_message = Post.where('id > ?', params[:post][:id]) } # json形式でアクセスがあった場合は、params[:message][:id]よりも大きいidがないかMessageから検索して、@new_messageに代入する
+    end
   end
 end
