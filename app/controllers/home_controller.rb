907930cf46b58ee74d7e4499722eb16d96d21044
care0717 <dev.care0717@gmail.com>
2017-10-22 14:35:12 +0900

    update: デザイン

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.all.order("created_at DESC")
     respond_to do |format|
       format.html # html形式でアクセスがあった場合は特に何もなし(@messages = Message.allして終わり）
       format.json { @new_message = Post.where('id > ?', params[:post][:id]) } # json形式でアクセスがあった場合は、params[:message][:id]よりも大きいidがないかMessageから検索して、@new_messageに代入する
